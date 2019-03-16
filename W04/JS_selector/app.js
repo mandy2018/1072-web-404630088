cd4bc7b656e87e66086e8f83e59d171e70a520fb
sr0214120 <sr0214120@gmail.com>
2019-03-16 16:50:00 +0800

    week4 Assignment

---
+++ W04/JS_selector/app.js
@@ -0,0 +1,8 @@
+alert('Hi'); //彈跳視窗
+let input = prompt('Enter your Name...');// 輸入彈跳視窗
+console.log(input);//存取輸入
+document.links[0].innerText="Hello"//改變超連結的 Text
+document.links[1].innerText="Hello1"//改變超連結的 Text
+document.querySelector('.hello')
+document.querySelector('#special')
+document.querySelectorAll('ul li')
